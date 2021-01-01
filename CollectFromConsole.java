268f4c50a6ac2e2c367172b7a19d6dd1448ebd38
Petrov-connect <Petrov.connect@gmail.com>
2021-01-01 19:21:08 +0200

    java code

--- CollectFromConsole.java
+++ CollectFromConsole.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 //created by J.M.
 public class CollectFromConsole {
@@ -7,29 +8,29 @@ public class CollectFromConsole {
 
         Scanner scan = new Scanner(System.in);
 
-        //split by one or more spaces :
-
-        // if we do not know data type :
-        Object[] objectArray= Arrays.stream(scan.nextLine().split("\\s+")).toArray();
-
-        // if we know data type :
-        //for [] :
-        // int :
+        // split by one or more spaces if we know data type :
+        // int [] :
         int[] intArray = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-        // String :
+        // String []:
         String [] stringArray= (String[]) Arrays.stream(scan.nextLine().split("\\s+")).toArray();
 
-        //for List :
-        // Integer:
+        // List Integer:
         List<Integer> listInteger= Arrays.stream(scan.nextLine().split("\\s+"))
                 .map(Integer::parseInt).collect(Collectors.toList());
-        //String:
+        // List String:
         List<String> listString= Arrays.stream((scan.nextLine().split("\\s+")))
         .collect(Collectors.toList());
+
+        //List Characters:
+        String text=scan.nextLine();
+        List<Character> charList= IntStream.range(0, text.length()).mapToObj(text::charAt).collect(Collectors.toList());
 
         //for map : key - name from console -> value =0;
         Map<String,Integer> mapInfo = Arrays.stream(scan.nextLine().split("\\s+"))
                 .collect(Collectors.toMap(s -> s, s -> 0, (a, b) -> a, LinkedHashMap::new));
+
+        //split by one or more spaces if we do not know data type:
+        Object[] objectArray= Arrays.stream(scan.nextLine().split("\\s+")).toArray();
 
     }
 }

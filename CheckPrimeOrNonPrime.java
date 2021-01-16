0692502fef0525a13f0174bd5bf9b7d581d2b342
Petrov-connect <Petrov.connect@gmail.com>
2021-01-16 15:24:05 +0200

    java code

--- CheckPrimeOrNonPrime.java
+++ CheckPrimeOrNonPrime.java
@@ -2,26 +2,27 @@ import java.util.Scanner;
 //created by J.M.
 
 public class CheckPrimeOrNonPrime {
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter integer:");
         int digit = Integer.parseInt(scan.nextLine());
-        for (int i = 2; i <= Math.abs(digit); i++) {
-            boolean prime = true;
-            int count = 0;
-            for (int j = 2; j <= digit; j++) {
-                if (i % j == 0) {
-                    count++;
-                }
-                if (count > 1) {
-                    prime = false;
-                }
+        boolean prime = true;
+
+        for (int i = 2; i <= Math.sqrt(Math.abs(digit)); i++) {
+
+            if (digit % i == 0) {
+                prime = false;
+                break;
             }
-            if (prime) {
-                System.out.println("Prime");
-            } else {
-                System.out.println("Non Prime");
-            }
+        }
+        if (prime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Non Prime");
         }
     }
 }
+
+

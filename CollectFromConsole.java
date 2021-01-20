29426e81c1e4f478c22a2c8a1b882fa750de41ea
Georgi Petrov <75274745+Petrov-connect@users.noreply.github.com>
2021-01-20 22:48:53 +0200

    Update CollectFromConsole.java

--- CollectFromConsole.java
+++ CollectFromConsole.java
@@ -8,18 +8,26 @@ public class CollectFromConsole {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
         // split by one or more spaces if we know data type :
+        
         // int [] :
         int[] intArray = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        
         // String []:
         String [] stringArray= scan.nextLine().split("\\s+");
+        
         // char[]:
         char[] arrayName = scan.nextLine().replaceAll("\\s+","").toCharArray();
+        
+        // double[]:
+        double[] arrayName = Arrays.stream(scan.nextLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
 
         // List Integer:
-        List<Integer> listInteger= Arrays.stream(scan.nextLine().split("\\s+"))
-                .map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> listInteger= Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+        
+        // List Double:
+        List<Double>arrayName=Arrays.stream(scan.nextLine().split("\\s+")).map(Double::parseDouble).collect(Collectors.toList());
+               
         // List String:
         List<String> listString= Arrays.stream((scan.nextLine().split("\\s+")))
         .collect(Collectors.toList());
@@ -29,8 +37,7 @@ public class CollectFromConsole {
         List<Character> charList= IntStream.range(0, text.length()).mapToObj(text::charAt).collect(Collectors.toList());
 
         //Queue: String:
-        ArrayDeque<String> queueName = Arrays.stream(scan.nextLine().split("\\s+"))
-                .collect(Collectors.toCollection(ArrayDeque::new));
+        ArrayDeque<String> queueName = Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toCollection(ArrayDeque::new));
         
         //Integer queue:
         ArrayDeque<Integer> deque = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toCollection(ArrayDeque::new));
@@ -40,11 +47,8 @@ public class CollectFromConsole {
         Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).forEach(numbersStack::push);
 
         //for map : key - name from console -> value =0;
-        Map<String,Integer> mapInfo = Arrays.stream(scan.nextLine().split("\\s+"))
-                .collect(Collectors.toMap(s -> s, s -> 0, (a, b) -> a, LinkedHashMap::new));
-
-        //Object:
-        Object[] objectArray= Arrays.stream(scan.nextLine().split("\\s+")).toArray();
-
+        Map<String,Integer> mapInfo = Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toMap(s -> s, s -> 0, (a, b) -> a, LinkedHashMap::new));  
+        
+        
     }
 }
